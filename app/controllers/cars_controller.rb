c574bbe3b9bbfba7f51fe40a9f83baf2fc31f229
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-09-05 14:13:46 +0200

    added bookings migrations

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,14 +1,10 @@
 class CarsController < ApplicationController
-
   def index
     @cars = Car.all
   end
 
   def show
     @car = Car.find(params[:id])
+    @booking = Booking.new
   end
-
 end
-
-# For the view, we need to include the car brand, model and year, and a photo and description of the car
-# There should also be a Book button that can be bootstrap
