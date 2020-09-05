dfee7813bc571bc956b12fa5dd8237953f55b217
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-09-05 10:46:09 +0200

    seeding done successfully

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 puts "Deleting previous records of users and cars"
 
-User.delete_all
 Car.delete_all
+User.delete_all
 
 puts "Generating new users and cars"
 
@@ -35,37 +35,38 @@ puts 'users created'
 
 puts 'creating cars...'
 
-def create_car(description, model, brand, year, user)
+def create_car(description, model, brand, year, price, user, file_path, file_name)
   car = Car.new({
-    title: "#{brand} #{model}",
-    description: description,
-    model: model,
-    brand: brand,
-    year: year,
-    user: user,
-  })
-  # car.photo.attach(io: File.open('path/to/file'), filename: '', content_type: 'image/png')
+      title: "#{brand} #{model}",
+      description: description,
+      model: model,
+      brand: brand,
+      year: year,
+      price: price,
+      user: user,
+    })
+  car.photo.attach(io: File.open(file_path), filename: file_name, content_type: 'image/jpg')
   car.save
 end
 
-create_car('the first fully electric compact SUV in Europe','Kona Electric', 'Hyundai', 2020, user1)
-create_car('was the world\'s top selling electric car until 2020','Leaf', 'Nissan', 2020, user2)
-create_car('an electric vehicle that won’t cost enthusiasts their souls','SE Electric Hardtop', 'Mini Cooper', 2020, user3)
-create_car('basically made of plastic','i3', 'BMW', 2020, user1)
-create_car('Crossover SUV''Niro EV', 'Kia', 2020, user2)
-create_car('doesn\'t look too bad for an Opel','Corsa-e', 'Opel', 2020, user3)
-create_car('one of the most efficient in its class','Ioniq Electric', 'Hyundai', 2020, user1)
-create_car('the first electric car from Jaguar','I-Pace', 'Jaguar', 2020, user2)
-create_car('tech geek but also a family man','Model X', 'Tesla', 2020, user3)
-create_car('your best choice if you don\'t like Tesla','e-tron', 'Audi', 2020, user1)
-create_car('you like living in the fast lane','Taycan', 'Porsche', 2020, user2)
-create_car('tech geeks first choice','Model 3', 'Tesla', 2020, user3)
-create_car('became the first electric car to top the monthly new-car-sales ranking in any country in 2013','Model S', 'Tesla', 2020, user2)
-create_car('basically an e-Golf but bigger','ID.3', 'Volkswagen', 2020, user1)
-create_car('sees right through your soul','e', 'Honda', 2020, user2)
-create_car('best selling EV in Europe according to Renault','Zoe', 'Renault', 2020, user3)
-create_car('a small EV perfect for city driving','e-Up', 'Volkswagen', 2020, user1)
-create_car('for those who want to really show off','EP9', 'Nio', 2020, user2)
+create_car('the first fully electric compact SUV in Europe','Kona Electric', 'Hyundai', 2020, 100, user1,  'app/assets/images/konaElectric.jpg', 'konaElectric.jpg')
+create_car('was the world\'s top selling electric car until 2020','Leaf', 'Nissan', 2020, 100, user2, 'app/assets/images/leaf.jpg', 'leaf.jpg')
+create_car('an electric vehicle that won’t cost enthusiasts their souls','SE Electric Hardtop', 'Mini Cooper', 2020, 100, user3, 'app/assets/images/minicooperSE.jpg', 'minicooperSE.jpg')
+create_car('basically made of plastic','i3', 'BMW', 2020, 100, user1, 'app/assets/images/i3.jpg', 'i3.jpg')
+create_car('Crossover SUV','Niro EV', 'Kia', 2020, 100, user2, 'app/assets/images/niroEV.jpg', 'niroEV.jpg')
+create_car('doesn\'t look too bad for an Opel','Corsa-e', 'Opel', 2020, 100, user3, 'app/assets/images/corsa-e.jpg', 'corsa-e.jpg')
+create_car('one of the most efficient in its class','Ioniq Electric', 'Hyundai', 2020, 100, user1, 'app/assets/images/ioniqE.jpg', 'ioniqE.jpg')
+create_car('the first electric car from Jaguar','I-Pace', 'Jaguar', 2020, 100, user2, 'app/assets/images/i-pace.jpg', 'i-pace.jpg')
+create_car('tech geek but also a family man','Model X', 'Tesla', 2020, 100, user3, 'app/assets/images/modelX.jpg', 'modelX.jpg')
+create_car('your best choice if you don\'t like Tesla','e-tron', 'Audi', 2020, 100, user1, 'app/assets/images/e-tron.jpg', 'e-tron.jpg')
+create_car('you like living in the fast lane','Taycan', 'Porsche', 2020, 100, user2, 'app/assets/images/taycan.jpg', 'taycan.jpg')
+create_car('tech geeks first choice','Model 3', 'Tesla', 2020, 100, user3, 'app/assets/images/model3.jpg', 'model3.jpg')
+create_car('became the first electric car to top the monthly new-car-sales ranking in any country in 2013','Model S', 'Tesla', 2020, 100, user2, 'app/assets/images/modelX.jpg', 'modelS.jpg')
+create_car('basically an e-Golf but bigger','ID.3', 'Volkswagen', 2020, 100, user1, 'app/assets/images/ID3.jpg', 'ID3.jpg')
+create_car('sees right through your soul','e', 'Honda', 2020, 100, user2, 'app/assets/images/honda-e.jpg', 'honda-e.jpg')
+create_car('best selling EV in Europe according to Renault','Zoe', 'Renault', 2020, 100, user3, 'app/assets/images/zoe.jpg', 'zoe.jpg')
+create_car('a small EV perfect for city driving','e-Up', 'Volkswagen', 2020, 100, user1, 'app/assets/images/E-Up.jpg', 'E-Up.jpg')
+create_car('for those who want to really show off','EP9', 'Nio', 2020, 100, user2, 'app/assets/images/EP9.jpg', 'EP9.jpg')
 
 print 'seeding done successfully!'
 
