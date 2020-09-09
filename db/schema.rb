b6c397fd7cc864bc442a06168320ed46cb6b7153
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-09-09 21:21:26 +0200

    added range to car, edited the controller and the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_114336) do
+ActiveRecord::Schema.define(version: 2020_09_09_185614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_114336) do
     t.integer "year"
     t.bigint "user_id", null: false
     t.integer "price"
+    t.integer "range"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
